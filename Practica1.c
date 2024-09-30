d04f68b986088a5fd6b6f3418e11b2f9195fbc10
KennethC18 <kennethcastillo18@gmail.com>
2024-09-29 18:44:03 -0600

    Función de reloj y función de alarma V1

--- Practica1.c
+++ Practica1.c
@@ -9,9 +9,56 @@
  * @file    Practica1.c
  * @brief   Application entry point.
  */
+
+#include "math.h"
 #include "Drivers/PIT.h"
 #include "Drivers/DAC.h"
 
-int main(void) {
+uint8_t sec, min, hour;
+uint32_t dacValue;
+uint32_t sineValues [20];
 
+void Clock_Time(void){
+	sec++;
+	if(sec == 60){
+		min++;
+		sec = 0;
+		if(min == 60){
+			hour++;
+			min = 0;
+			if(hour == 24){
+				hour = 0;
+			}
+		}
+	}
+}
+
+void Get_Sine_Values(void){
+	for (int i = 0; i < 360; ++i) {
+		if(i % (360 / 20) != 0){
+			sineValues[i] = (uint32_t)floor(2047 * sin(i) + 2047);
+		}
+	}
+}
+
+void Alarm(void){
+	uint8_t i = 0;
+
+	//10khz, 5us por muestra, 20 muestras
+	dacValue = sineValues[(i == 20) ? (i = 0) : (i++)];
+	DAC_Update_Val(dacValue);
+}
+
+int main(void) {
+	PIT_Start();
+	DAC_Start();
+	Get_Sine_Values();
+
+	PIT_Change_Period(USEC_TO_COUNT(1000000U, PIT_SOURCE_CLOCK), kPIT_Chnl_0);
+	PIT_SetCallback(Clock_Time, kPIT_Chnl_0);
+
+	PIT_Change_Period(USEC_TO_COUNT(5U, PIT_SOURCE_CLOCK), kPIT_Chnl_1);
+	PIT_SetCallback(Alarm, kPIT_Chnl_1);
+
+	return 0;
 }
